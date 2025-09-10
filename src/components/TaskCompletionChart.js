0ccd0020114260fe7ebe51beebb0c6cb12f6213b
charity <Charitymuthoka15@gmail.com>
2025-09-10 15:25:28 +0300

    new changes to task completion graph

--- src/components/TaskCompletionChart.js
+++ src/components/TaskCompletionChart.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, BarElement, CategoryScale, LinearScale } from 'chart.js';
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
 
 ChartJS.register(BarElement, CategoryScale, LinearScale);
 
@@ -12,25 +17,69 @@ const TaskCompletionChart = ({ data }) => {
         label: 'Completion %',
         data: data.map((d) => d.completion),
         backgroundColor: '#00b5ad',
-        borderRadius: 4,
+        borderRadius: 6, 
+        barPercentage: 0.9,      
+        categoryPercentage: 0.8, 
       },
     ],
   };
 
   const options = {
     responsive: true,
+    maintainAspectRatio: false,
     scales: {
+      x: {
+        grid: {
+          display: false, 
+        },
+        border: {
+          display: true, 
+        },
+        ticks: {
+          font: {
+            size: 12,
+            weight: '500',
+           
+
+          },
+          color: '#333',
+        },
+      },
       y: {
         beginAtZero: true,
         max: 100,
+        grid: {
+          display: false, 
+        },
+        border: {
+          display: true, 
+        },
+        ticks: {
+          stepSize: 20,
+          font: {
+            size: 12,
+          },
+          color: '#333',
+         
+
+        },
       },
     },
     plugins: {
-      legend: { display: false },
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: true,
+      },
     },
   };
 
-  return <Bar data={chartData} options={options} />;
+  return (
+    <div style={{ height: '300px' }}>
+      <Bar data={chartData} options={options} />
+    </div>
+  );
 };
 
 export default TaskCompletionChart;
